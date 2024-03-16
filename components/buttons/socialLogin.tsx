77273dc193fde90ffe686113fe22489c939070b3
itsunknown01 <aryamangohain@gmail.com>
2024-03-16 19:51:38 +0530

    OAuth setup

--- components/buttons/socialLogin.tsx
+++ components/buttons/socialLogin.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+"use client";
+
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { signIn } from "next-auth/react";
+
+import { Button } from "../ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const SocialLogin = () => {
-  return (
-    <div>
-      TODO: O-Auth Login
-    </div>
-  )
-}
+  const handleClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
 
-export default SocialLogin
+  return (
+    <div className="flex items-center w-full gap-x-2">
+      <Button
+        className="w-full"
+        size="lg"
+        variant="outline"
+        onClick={() => handleClick("google")}
+      >
+        <FcGoogle className="w-5 h-5" />
+      </Button>
+      <Button
+        className="w-full"
+        size="lg"
+        variant="outline"
+        onClick={() => handleClick("github")}
+      >
+        <FaGithub className="w-5 h-5" />
+      </Button>
+    </div>
+  );
+};
+
+export default SocialLogin;
